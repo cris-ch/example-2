2851f4c730a75ce55a502071fdcb73ebf534bad6
Cristian <cristian.christensen@gmail.com>
2022-02-01 15:41:13 +0100

    Add paragraph

--- view.test.js
+++ view.test.js
@@ -16,13 +16,13 @@ describe('Page view', () => {
 
   // addParagraph
   // create a new p element and store it in a variable.
-  // describe('addParagraph', () => {
-  //   it('creates a p element and stores', () => {
-  //     document.body.innerHTML = fs.readFileSync('./index.html');
+  describe('addParagraph', () => {
+    it('creates a p element and stores', () => {
+      document.body.innerHTML = fs.readFileSync('./index.html');
       
-  //     const view = new View();
-  //     expect(view.addParagraph('Hello')).toEqual()
-
-  //   })
-  // })
+      const view = new View();
+      view.addParagraph();
+      expect(document.querySelectorAll('p').length).toBe(3);
+    })
+  })
 });
